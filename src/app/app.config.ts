eacc182afc46db21abd71f02925367588208512d
Bruno <bruno.rp45@gmail.com>
2025-02-02 09:56:50 -0300

    ✨ feat(Styling): Add Angular Material theme and typography styles

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,7 +5,8 @@ import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay()), provideAnimations()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay()), provideAnimations(), provideAnimationsAsync()]
 };
