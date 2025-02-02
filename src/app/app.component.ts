8c997f9a6cb77f972a5b63b3657802007998788d
Bruno <bruno.rp45@gmail.com>
2025-02-02 11:30:06 -0300

    ✨ feat(Servicos): Add Servicos component with service offerings and integrate into app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import { HeroSectionComponent } from "./components/hero-section/hero-section.component";
+import { ServicosComponent } from "./components/servicos/servicos.component";
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeroSectionComponent],
+  imports: [RouterOutlet, HeroSectionComponent, ServicosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/components/servicos/servicos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MatTabsModule } from '@angular/material/tabs';
+
+@Component({
+  selector: 'app-servicos',
+  imports: [MatTabsModule],
+  templateUrl: './servicos.component.html',
+  styleUrl: './servicos.component.scss'
+})
+export class ServicosComponent {
+
+}
