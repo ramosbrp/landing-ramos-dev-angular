2fd656f32c89a452af0fbf497adf4b5a1e51b55e
Bruno <bruno.rp45@gmail.com>
2025-02-03 23:08:43 -0300

    ✨ feat(Sobre): Add Sobre component and integrate into app; update hero section and contact form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 
 import { HeroSectionComponent } from "./components/hero-section/hero-section.component";
 import { ServicosComponent } from "./components/servicos/servicos.component";
+import { SobreComponent } from "./components/sobre/sobre.component";
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeroSectionComponent, ServicosComponent],
+  imports: [RouterOutlet, HeroSectionComponent, ServicosComponent, SobreComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/components/button-cta/button.cta.ts
+++ src/app/components/button-cta/button.cta.ts
@@ -1,30 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { trigger, style, transition, animate, state } from '@angular/animations';
 
 @Component({
   selector: 'button-cta',
   standalone: true,
   template: `  
-    @if (isVisible) {
-      <button [@fadeIn]="isVisible ? 'visible' : 'hidden'" >Solicitar Orçamento Grátis</button>
-    }
+    <button  [@fadeIn]="animationState()">Solicitar Orçamento Grátis</button>
   `,
   styleUrl: './button.cta.scss',
   animations: [
     trigger('fadeIn', [
-      state('hidden', style({ opacity: 0, transform: 'translateY(30px)' })),
+      state('hidden', style({ opacity: 0, transform: 'translateY(200%)' })),
       state('visible', style({ opacity: 1, transform: 'translateY(0)' })),
       transition('hidden => visible', [
-        style({ opacity: 0, transform: 'translateX(30px)' }), // Garante que começa do estado certo
         animate('1s ease-out')])
     ])
   ],
 })
 export class ButtonCta {
-  isVisible = false;
+  animationState = signal<'hidden' | 'visible'>('hidden');
+
   ngOnInit() {
     setTimeout(() => {
-      this.isVisible = true;
-    }, 3000); // Delay de 3 segundos antes da animação começar
+      this.animationState.set('visible');
+    }, 1000);
   }
 }
